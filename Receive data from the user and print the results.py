251c1686adadc19158505f1a16b42b244a78fbce
LidaRP <rahmanipiyani.lida@gmail.com>
2022-05-06 22:08:45 +0430

    print and input.py

---
+++ Receive data from the user and print the results.py
@@ -0,0 +1,14 @@
+a = int(input('Please enter first number : '))
+b = int(input('Please enter second number : '))
+
+summ = a+b
+minus = a-b
+multi = a*b
+div = a/b
+
+print('sum of your numbers is : ',summ)
+print('minus of your numbers is : ',minus)
+print('multiple of your numbers is : ',multi)
+print('divide of your numbers is : ',div)
+
+              
